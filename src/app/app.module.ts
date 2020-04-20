de4e544fc0555832e52126303f2c952ebb239e75
tarunrrathi <tarunr@techfoursolutions.com>
2020-04-20 22:16:45 +0530

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent} from './posts/post-create/post-create.component';
 import { HelloComponent } from './hello.component';
+import{BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,BrowserAnimationsModule,MatFormFieldModule,MatInputModule,MatCardModule ],
   declarations: [ AppComponent, HelloComponent,PostCreateComponent ],
   bootstrap:    [ AppComponent ]
 })
